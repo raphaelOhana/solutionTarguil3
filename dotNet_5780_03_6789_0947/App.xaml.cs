ac269005a226f1db140399dda302a0d2d634940f
raphael ohana <rohana@g.jct.ac.il>
2019-12-01 17:44:55 +0200

    ici copier tout les fichier

---
+++ dotNet_5780_03_6789_0947/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace dotNet_5780_03_6789_0947
+{
+    /// <summary>
+    /// Logique d'interaction pour App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
